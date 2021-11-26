136f1ea3353decf6e30be44f46de37238f91d066
copokopy4ka <copokopy4ka739@gmail.com>
2021-11-26 11:40:47 +0200

    added login modal

--- src/js/script.js
+++ src/js/script.js
@@ -67,6 +67,61 @@ setTimeout(addClass, 700, introSubTitle);
 setTimeout(addClass, 1100, introBtn);
 setTimeout(addClass, 1200, introLink);
 
+const body = document.querySelector('body');
+const loginBtn = document.querySelector('.header__btn_login');
+const loginModal = document.querySelector('.login-modal');
+const loginModalBtn = document.querySelector('.login-modal__btn');
+const loginModalClose = document.querySelector('.login-modal__close');
+const changeModal = document.querySelector('.change-modal');
+const loginModalText = document.querySelector('.login-modal__text');
+const loginModalTitile = document.querySelector('.login-modal__title');
+
+loginBtn.addEventListener('click', () => {
+  loginModal.classList.add('showed');
+  body.classList.add('no-scroll');
+  header.classList.add('modal-opened');
+})
+
+loginModalClose.addEventListener('click', () => {
+  loginModal.classList.remove('showed');
+  body.classList.remove('no-scroll');
+  header.classList.remove('modal-opened');
+})
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('no-scroll')) {
+    loginModal.classList.remove('showed');
+    body.classList.remove('no-scroll');
+    header.classList.remove('modal-opened');
+  }
+})
+
+changeModal.addEventListener('click', (event) => {
+  loginModal.classList.add('change-modal');
+
+  if(loginModalTitile.innerText === 'Log in') {
+    setTimeout(() => {
+      loginModalText.innerText = 'Already have an account?';
+      changeModal.innerText = 'Log in';
+      loginModalBtn.innerText = 'Sing up';
+      loginModalTitile.innerText = 'Sing up';
+    }, 400);
+  } else {
+    setTimeout(() => {
+      loginModalText.innerText = 'Need an account?';
+      changeModal.innerText = 'Sing up';
+      loginModalBtn.innerText = 'Log in';
+      loginModalTitile.innerText = 'Log in';
+    }, 400);
+  }
+
+  
+
+  setTimeout(() => {
+    loginModal.classList.remove('change-modal');
+  }, 850);
+  
+})
 $(function(){
   $('.products__slider').slick({
     autoplay: true,
